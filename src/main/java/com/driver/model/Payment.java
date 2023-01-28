9548e6267bb4f7553e4f93c74fedbe10814326f3
Pragati <pragati.sri2411@gmail.com>
2023-01-28 17:34:53 +0530

    second commit

--- src/main/java/com/driver/model/Payment.java
+++ src/main/java/com/driver/model/Payment.java
@@ -32,7 +32,7 @@ public class Payment {
         this.id = id;
     }
 
-    public Boolean getPaymentCompleted() {
+    public Boolean isPaymentCompleted() {
         return paymentCompleted;
     }
 
